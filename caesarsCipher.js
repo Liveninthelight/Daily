7e8097b96e957347abbb42f0f4650909de9bfc33
Joe Keenan <JoeKeenan22@me.com>
2018-05-16 15:54:48 -0400

    Algorithms

---
+++ caesarsCipher.js
@@ -0,0 +1,24 @@
+
+function rot13(str) { // LBH QVQ VG!
+  
+  var strSep = str.split(""); // Array
+   
+  for (i = 0; i < strSep.length; i++) {
+    strSep[i] = strSep[i].charCodeAt(0);
+    if (strSep[i] > 64 && strSep[i] < 78 ) {
+       strSep[i] += 13;
+    } else if (strSep[i] > 77 && strSep[i] < 91) {
+       strSep[i] -= 13;
+      }
+    strSep[i] = String.fromCharCode(strSep[i]);
+   
+    
+  }
+       
+  
+  
+  return strSep.join("");
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
